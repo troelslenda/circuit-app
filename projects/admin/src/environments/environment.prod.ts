edbfa0486224b3d19e083655e53f194ff1d3fe62
Troels Lenda Strand <troelslenda@gmail.com>
2020-05-25 19:49:34 +0200

    adding basic travis config

--- projects/admin/src/environments/environment.prod.ts
+++ projects/admin/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCoEC9gZaY7_lO29tSG8a8Fyc5FP-1lPiE",
+    authDomain: "dgi-it-eef5d.firebaseapp.com",
+    databaseURL: "https://dgi-it-eef5d.firebaseio.com",
+    projectId: "dgi-it-eef5d",
+    storageBucket: "dgi-it-eef5d.appspot.com",
+    messagingSenderId: "490195614309",
+    appId: "1:490195614309:web:9c82f2f805917c9489248c"
+  }
 };
